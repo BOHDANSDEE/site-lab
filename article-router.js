f6aea9164cc4800f4f40fef62c4e6cdc31375b74
BOHDANSDEE <boday.solodov@gmail.com>
2026-08-06 13:36:23 +0300

    Підключити повну статтю про мотивацію

--- article-router.js
+++ article-router.js
@@ -1,7 +1,26 @@
 (() => {
   const slug = location.pathname.split('/').filter(Boolean).pop();
-  const procrastinationRichSlugs = new Set(["prokrastynatsiia-i-perfektsionizm", "akademichna-prokrastynatsiia", "prokrastynatsiia-i-tryvoha", "chomu-vse-roblu-v-ostanniu-myt", "sduh-i-prokrastynatsiia", "metod-pomodoro", "nichna-prokrastynatsiia", "telefon-korotki-video-i-prokrastynatsiia"]);
-  const lazinessRichSlugs = new Set(["prychyny-lini", "lin-chy-vyhorannia", "yak-zmusyty-sebe-vchytysia", "yak-zmusyty-sebe-prybyraty", "lin-u-pidlitkiv", "yak-vstaty-z-lizhka-vrantsi", "chomu-pislia-roboty-nichoho-ne-khochetsia", "yak-rozvynuty-samodystsyplinu"]);
+  const procrastinationRichSlugs = new Set([
+    'prokrastynatsiia-i-perfektsionizm',
+    'akademichna-prokrastynatsiia',
+    'prokrastynatsiia-i-tryvoha',
+    'chomu-vse-roblu-v-ostanniu-myt',
+    'sduh-i-prokrastynatsiia',
+    'metod-pomodoro',
+    'nichna-prokrastynatsiia',
+    'telefon-korotki-video-i-prokrastynatsiia'
+  ]);
+  const lazinessRichSlugs = new Set([
+    'prychyny-lini',
+    'lin-chy-vyhorannia',
+    'yak-diiaty-koly-nemaie-motyvatsii',
+    'yak-zmusyty-sebe-vchytysia',
+    'yak-zmusyty-sebe-prybyraty',
+    'lin-u-pidlitkiv',
+    'yak-vstaty-z-lizhka-vrantsi',
+    'chomu-pislia-roboty-nichoho-ne-khochetsia',
+    'yak-rozvynuty-samodystsyplinu'
+  ]);
 
   const script = document.createElement('script');
   script.src = lazinessRichSlugs.has(slug)
