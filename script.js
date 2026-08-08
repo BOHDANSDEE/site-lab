63513edeb48e249e3e15acf767a843528c38ffae
BOHDANSDEE <boday.solodov@gmail.com>
2026-08-08 16:59:27 +0300

    Підключити оновлений каталог апатії

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ const loadScript = (src) => new Promise((resolve, reject) => {
 loadScript('/articles-index.js')
   .then(() => loadScript('/article-topic-overrides.js'))
   .then(() => loadScript('/lazy-topic-overrides.js'))
+  .then(() => loadScript('/apathy-topic-overrides.js'))
   .then(() => loadScript('/phone-theme-update.js'))
   .then(() => loadScript('/catalog-30.js'))
   .then(() => import('/script-base.js'))
