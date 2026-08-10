f79f3d276c755b3ab9006de9fc154188317d7462
BOHDANSDEE <boday.solodov@gmail.com>
2026-08-10 16:26:16 +0300

    Підключити розширений каталог 45 статей

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ const loadSiteScripts = () =>
     .then(() => loadScript('/article-topic-overrides.js'))
     .then(() => loadScript('/lazy-topic-overrides.js'))
     .then(() => loadScript('/apathy-topic-overrides.js'))
+    .then(() => loadScript('/articles-index-45.js'))
     .then(() => loadScript('/phone-theme-update.js'))
     .then(() => loadScript('/catalog-30.js'))
     .then(() => import('/script-base.js'));
