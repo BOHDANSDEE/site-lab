a5747909b47544f9c5d20ae46f33ad055d16a802
BOHDANSDEE <boday.solodov@gmail.com>
2026-08-23 15:38:50 +0300

    Додати список 9 вибраних статей

---
+++ selected-articles.js
@@ -0,0 +1,24 @@
+(() => {
+  const selected = [
+    ['yak-poboroty-lin', 'Як побороти лінь'],
+    ['prychyny-lini', 'Чому виникає лінь і що з цим робити'],
+    ['yak-diiaty-koly-nemaie-motyvatsii', 'Що робити, коли немає мотивації'],
+    ['yak-vstaty-z-lizhka-vrantsi', 'Як прокидатися зранку'],
+    ['apatiia-u-pidlitkiv', 'Апатія у підлітків: ознаки, причини та коли потрібна допомога'],
+    ['nichogo-ne-raduie-yak-povernuty-interes-do-zhyttia', 'Нічого не радує: як повернути інтерес до життя'],
+    ['apatiia-shcho-robyty', 'Апатія: що робити, коли нічого не хочеться'],
+    ['yak-vybraty-priorytet-koly-vse-terminove', 'Як вибирати пріоритети'],
+    ['sduh-i-prokrastynatsiia', 'Як зосередитися на навчанні']
+  ];
+
+  const bySlug = new Map(selected);
+  const source = window.HABITTEEN_ARTICLE_INDEX || [];
+  const byCurrentSlug = new Map(source.map((item) => [item.slug, item]));
+
+  window.HABITTEEN_ARTICLE_INDEX = selected
+    .map(([slug, title]) => {
+      const item = byCurrentSlug.get(slug);
+      return item ? Object.assign({}, item, { title }) : null;
+    })
+    .filter(Boolean);
+})();
