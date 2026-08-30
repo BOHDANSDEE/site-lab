6c91ae4e600aecf9bffd0de81414ce5cce7bcbfb
BOHDANSDEE <boday.solodov@gmail.com>
2026-08-30 14:17:53 +0300

    Оновити імпорти даних Ліні після перенесення

--- api/lin-experiment-data.mjs
+++ api/lin-experiment-data.mjs
@@ -1,7 +1,7 @@
 import { LIN_ARTICLES_1 } from '../article-data/lin-experiment-1.mjs';
 import { LIN_ARTICLES_2 } from '../article-data/lin-experiment-2.mjs';
-import { LIN_ARTICLES_3 } from './lin-experiment-3.mjs';
-import { LIN_ARTICLES_4 } from './lin-experiment-4.mjs';
+import { LIN_ARTICLES_3 } from '../article-data/lin-experiment-3.mjs';
+import { LIN_ARTICLES_4 } from '../article-data/lin-experiment-4.mjs';
 export { LIN_GROUPS, LIN_SOURCES } from '../article-data/lin-experiment-common.mjs';
 
 export const LIN_ARTICLES = [...LIN_ARTICLES_1, ...LIN_ARTICLES_2, ...LIN_ARTICLES_3, ...LIN_ARTICLES_4];
